4a0cc98c1a4fd9d3ee7afae8356cd02f4a535133
Diaz Rabadan, Diego <ddiaz@eulen.com>
2022-04-18 11:35:47 +0200

    initial commit

---
+++ src/main/java/com/ddr/springboot/app/config/SpringbootServicioDdrPostgresqlApplication.java
@@ -0,0 +1,13 @@
+package com.ddr.springboot.app.config;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootServicioDdrPostgresqlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootServicioDdrPostgresqlApplication.class, args);
+	}
+
+}
